ba37aaa7b96fa38fa896c1373920565f39562aa0
Max van Asten <maxvasten@gmail.com>
2022-06-30 16:05:26 +0200

    Base structure

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//
+#!/usr/bin/env node
+const path = require("node:path");
+
+// Initiate server
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+// Serve website using EJS
+app.get("/", function (req, res) {
+  res.render("website/index");
+});
+
+// Run server
+app.listen(80);
+console.log("Server is listening on port 80");
