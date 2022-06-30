ba8f4803f0ddb1c28e915bbca795a6bb05fa238e
Max van Asten <maxvasten@gmail.com>
2022-06-30 16:36:13 +0200

    Improved off-site development

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const path = require("node:path");
+const PORT = 3000;
 
 // Initiate server
 const express = require("express");
@@ -12,6 +13,10 @@ app.get("/", function (req, res) {
   res.render("main/index");
 });
 
+app.get("/test", function (req, res) {
+  res.send("TEST");
+});
+
 // Run server
-app.listen(80);
-console.log("Server is listening on port 80");
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
